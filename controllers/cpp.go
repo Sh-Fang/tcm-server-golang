5690c430157bad6239de09dde015cd18517a50b5
shengoasis <shengoasis@gmail.com>
2025-03-21 23:41:27 +0800

    feat：完成了子图匹配的api

--- controllers/cpp.go
+++ controllers/cpp.go
@@ -3,67 +3,115 @@ package controllers
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strings"
-	"tcm-server-go/services" // 引入你定义的服务
 
+	// 引入你定义的服务
 	"github.com/gin-gonic/gin"
 )
 
 // 调用 C++ /match 接口
 func CallMatch(c *gin.Context) {
-	// 从请求中获取数据（假设是 JSON 格式）
-	var data map[string]interface{}
-	if err := c.ShouldBindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
-		return
-	}
-
-	// 将 map 数据序列化为 JSON 格式的字节数组
-	dataBytes, err := json.Marshal(data)
+	// 获取上传的文件
+	streamGraph, err := c.FormFile("streamGraph") // "streamGraph" 是前端上传的字段名
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal data"})
+		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
-	// 调用 C++ API
-	response, err := services.CallMatchAPI(dataBytes)
+	queryGraph, err := c.FormFile("queryGraph")
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var params map[string]any
+	paramsStr := c.PostForm("params")
+	if err := json.Unmarshal([]byte(paramsStr), &params); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid JSON parameters"})
+		return
+	}
+
+	// 把所有非字符串的参数转换成字符串类型，如果本身字符串是"true"或"false"，则转换成"y"或"n"
+	for key, value := range params {
+		if _, ok := value.(string); !ok {
+			if value == true {
+				params[key] = "y"
+			} else if value == false {
+				params[key] = "n"
+			} else {
+				params[key] = fmt.Sprintf("%v", value)
+			}
+		}
+	}
+
+	// 目标路径
+	streamFilePath := "/tmp/tcm_download/" + streamGraph.Filename
+	queryFilePath := "/tmp/tcm_download/" + queryGraph.Filename
+
+	if err := c.SaveUploadedFile(streamGraph, streamFilePath); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.SaveUploadedFile(queryGraph, queryFilePath); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	params["stream_path"] = streamFilePath
+	params["query_path"] = queryFilePath
+
+	// 将params封装为 JSON
+	jsonParams, _ := json.Marshal(params)
+
+	// 发送 POST 请求到 C++ 后端
+	response, err := http.Post("http://localhost:8081/match", "application/json", strings.NewReader(string(jsonParams)))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	defer response.Body.Close()
 
 	// 解析 C++ API 返回的 JSON 字符串
 	var responseData map[string]interface{}
-	err = json.Unmarshal([]byte(response), &responseData)
+	err = json.NewDecoder(response.Body).Decode(&responseData)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse response"})
 		return
 	}
 
+	// 删除临时文件
+	os.Remove(streamFilePath)
+	os.Remove(queryFilePath)
+
 	// 返回 C++ API 的响应
 	c.JSON(http.StatusOK, responseData)
+
 }
 
 // 调用 C++ /progress 接口
 func CallProgress(c *gin.Context) {
-	// 调用 C++ API
-	response, err := services.CallProgressAPI()
+	url := "http://localhost:8081/progress"
+	resp, err := http.Get(url)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+	}
+	defer resp.Body.Close()
+
+	// 读取响应内容
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 
-	// 解析 C++ API 返回的 JSON 字符串
-	var responseData map[string]interface{}
-	err = json.Unmarshal([]byte(response), &responseData)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse response"})
-		return
-	}
+	fmt.Println(string(body))
 
 	// 返回 C++ API 的响应
-	c.JSON(http.StatusOK, responseData)
+	c.JSON(http.StatusOK, gin.H{"progress": string(body)})
 }
 
 func AnalyzeStreamGraph(c *gin.Context) {
